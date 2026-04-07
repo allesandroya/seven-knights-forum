ab16d00f9cb72090ae8e613823e31f2863f4ca34
allesandroya <allesandroya@gmail.com>
2026-04-07 12:11:28 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Change 'komunitas-forum' to your actual GitHub repo name
-  base: '/komunitas-forum/',
+  base: '/seven-knights-forum/',
 })
